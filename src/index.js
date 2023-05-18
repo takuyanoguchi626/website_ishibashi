6c3d8fdf577e9c78d8ddb738a5b08bcd5e7e6a33
takuyanoguchi626 <ngctky626@gmail.com>
2023-05-18 14:47:55 +0900

    fix header hamburger_menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./scss/style.scss"
 
 console.log("Hello World!");
+
+
+const hamburger_menu = document.getElementsByClassName("js_hamburger_menu")[0];
+const hamburger_menu_border = document.getElementsByClassName("js_hamburger_menu_border")[0];
+const header_navi_sp = document.getElementsByClassName("js_header_navi_sp")[0];
+
+hamburger_menu.addEventListener("click", function () {
+    hamburger_menu_border.classList.toggle("is_active");
+    header_navi_sp.classList.toggle("is_active");
+})
