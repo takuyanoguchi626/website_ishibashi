8e6d7d8db7783d195e96812b41cfbe17dac5ed2b
takuyanoguchi626 <ngctky626@gmail.com>
2023-05-18 20:35:19 +0900

    fix lower_page first_view

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import "./scss/style.scss"
 
 console.log("Hello World!");
 
+// const body = document.getElementsByClassName("hidden")[0];
+
+// window.onload = function () {
+//     setTimeout(() => {
+//         body.classList.remove("hidden");
+//     }, 2000);
+// }
+
 
 const hamburger_menu = document.getElementsByClassName("js_hamburger_menu")[0];
 const hamburger_menu_border = document.getElementsByClassName("js_hamburger_menu_border")[0];
